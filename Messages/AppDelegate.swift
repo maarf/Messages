69f88423849bca4c34049383e979c0adc9c1521c
Martins Spilners <martins@ctrld.ch>
2020-02-25 11:24:23 +0200

    Initial Commit

---
+++ Messages/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Messages
+//
+//  Created by Martins on 25/02/2020.
+//  Copyright © 2020 Martins Spilners. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Messages/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Messages
+//
+//  Created by Martins on 25/02/2020.
+//  Copyright © 2020 Martins Spilners. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
